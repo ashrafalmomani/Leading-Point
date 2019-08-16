cdfb312984a7f70f865baa4fd66d0bb518ffda1f
mahmoud <mahmoud_daher09@hotmail.com>
2019-08-16 19:30:41 +0300

    the new commit

--- hr_custom/models/account_asset.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class AccountAsset(models.Model):
-    _inherit = 'account.asset.asset'
-
-    is_tracked = fields.Boolean("Is tracked?")
--- project_custom/models/__init__.py
+++
@@ -1,3 +0,0 @@
-
-from . import project
-from . import account_analytic_line
